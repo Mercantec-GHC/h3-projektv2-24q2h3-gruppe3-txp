d3a332d5d619a59c7dd78b93efcb811d91dcd32f
Druumihra <teis.bindner@laerke.net>
2024-04-25 08:57:59 +0200

    added pageination functionality

--- Frontend/user_interface/build.rs
+++ Frontend/user_interface/build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    slint_build::compile("ui/login.slint").unwrap();
+    slint_build::compile("ui/mainwindow.slint").unwrap();
 }
--- Frontend/user_interface/src/main.rs
+++ Frontend/user_interface/src/main.rs
@@ -1,95 +1,77 @@
 use std::{collections::HashMap, env};
-use reqwest::{Client, Error, Response};
-use slint::platform::Key;
-use slint::{EventLoopError, JoinHandle};
-use tokio::runtime;
+use reqwest::{Error, Response};
+use serde::{Deserialize, Serialize};
+
 slint::include_modules!();
-// use std::thread;
-// use std::time::Duration;
 
-// fn main() {
-//     thread::spawn(|| {
-//         for i in 1..10 {
-//             println!("hi number {} from the spawned thread!", i);
-//             thread::sleep(Duration::from_millis(1));
-//         }
-//     });
 
-//     for i in 1..5 {
-//         println!("hi number {} from the main thread!", i);
-//         thread::sleep(Duration::from_millis(1));
-//     }
-// }
 
+#[derive(Serialize,Deserialize)]
+struct ServerResponse {
+    message: String,
+}
+
+#[derive(Serialize,Deserialize)]
+struct LoginCredentials {
+    username: String,
+    password: String,
+}
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     
+    
     ui.on_request_login({
         move |data: User| {
-            let username:slint::SharedString = data.username;
+            let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
+            slint::spawn_local( async move {
+
+            })
             //should send username and hashed password to api at {API-CON-STRING}
             //after awaiting and recieving a response it should either log in and open up the dashboard
             //or an error message should pop up asking the user to try again or create a new account
+
         }
     });
+    // create_user(data);
+    //It should take a username, and password, where it checks whether the first password is the same
+    //should check if the data is proper
+    //as the second after which it should send a post request to api and then "redirect" to login page
+    //while displaying a confirmation of some sort.
     ui.on_request_create_user({
         move |data: User| {
+            print!("{data:?}");
             //needs to use slint::spawn_local()
             //need to figure out how to use said functionality
+            //creates a new user in the database
+            let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
             slint::spawn_local( async move {
-                    let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
-                    let result = tokio_runtime.spawn(async move {
+                    let res: Result<Response,Error>  = tokio_runtime.spawn(async move {
                         // This code is running on the Tokio runtime's thread, you can await futures that depends on Tokio here.
                         let mut map = HashMap::new();
-                        map.insert("username", "data.username");
-                        map.insert("password", "data.password");
+                        map.insert("username", data.username.to_string());
+                        map.insert("password", data.password.to_string());
                         let client = reqwest::Client::new();
-                        let response = client.post("https://172.16.113.50:4321/createUser").json(&map).send().await;
-                        print!("{response:?}")
-                        
+                        // fn test() -> String{
+                        //     return format!(
+                        //         "{}{}",
+                        //         env::var("DB_CON").unwrap(), "createUser"
+                        //     )
+                        // }
+                        let response = client.post("https://h3-projektv2-24q2h3-gruppe3-txp.onrender.com/createUser").json(&map).send().await;
+                        return response;
                     }).await.unwrap(); 
-                    //assert_eq!(result, )
-                }).unwrap();
+                    if res.is_ok() {
+                        //ui.set_usercreated(true)
 
-                // thread::spawn(|| {
-                //     let mut map = HashMap::new();
-                //     map.insert("username", "data.username");
-                //     map.insert("password", "data.password");
-                //     let Client = reqwest::Client::new();
-                //     let response = Client.post("http://172.16.113.50:4321/createUser")
-                //     .json(&map)
-                //     .send();
-                //     print!("test1");
-                // });
-                
-                // create_user(data);
-                //It should take a username, and password, where it checks whether the first password is the same
-                //should check if the data is proper
-                //as the second after which it should send a post request to api and then "redirect" to login page
-                //while displaying a confirmation of some sort.
-                print!("test2");
-                return(true);
+                    }
+                    
+                   //let result = from_str::<server_response>(result);
+                }).unwrap();     
             }
         });
+        
         ui.run()
     }
 
 
 
-
-    // async fn create_user(data:User) {
-    //     // let passcode = env::var(constring)
-    //     // let request_url: = env::var("DB_CON");
-    //     let mut map = HashMap::new();
-    //     map.insert("username", data.username);
-    //     map.insert("password", data.password);
-    //     let Client = reqwest::Client::new();
-    //     let response = Client.post("http://172.16.113.50:4321/createUser")
-    //     // .basic_auth(gh_user.clone(), Some(gh_pass.clone()))
-    //     .json(&map)
-    //     .send().await;
-    //     // return match response.status()  {
-    //     //     Err(e) => return Err(e),
-    //     //     Some(t) => return Ok(t),
-    //     // }
-    // }
